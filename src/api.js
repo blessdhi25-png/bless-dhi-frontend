27cab4b68a8bc6e55112f025e83012c666cf70c9
Bless Adika <blessdhi.25@gmail.com>
2026-05-22 20:38:36 +0000

    Initial frontend commit

--- src/api.js
+++ src/api.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : '/api'
+});
 
-// Attach JWT token to every request
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
-// Redirect to login on 401
 api.interceptors.response.use(
   res => res,
   err => {
